d8f93d4d1a974df136519796fd297ec0af52c397
Tibus <julien.delnatte@gmail.com>
2016-06-20 13:43:15 +0200

    add build for mac on new electron version

--- build.sh
+++ build.sh
@@ -6,17 +6,17 @@
 version="$(jq -r '.version' src/package.json)"
 versionUnderscore=${version//./_}
 
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${version}" "./build/electron-v0.36.4-darwin-x64/DagomaBurner.app/Contents/Info.plist"
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${version}" "./build/electron-v0.36.4-darwin-x64/DagomaBurner.app/Contents/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${version}" "./build/electron-v1.1.3-darwin-x64/DagomaBurner.app/Contents/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${version}" "./build/electron-v1.1.3-darwin-x64/DagomaBurner.app/Contents/Info.plist"
 
 
 # Windows
-asar pack src/ app.asar
-cp app.asar ./build/electron-v0.36.4-win32-x64/resources/
+#asar pack src/ app.asar
+#cp app.asar ./build/electron-v0.36.4-win32-x64/resources/
 
-cp -R "./build/electron-v0.36.4-win32-x64" "./build/DagomaBurner ${versionUnderscore}"
+#cp -R "./build/electron-v0.36.4-win32-x64" "./build/DagomaBurner ${versionUnderscore}"
 
 # MAC
-#asar pack src/ app.asar
-#cp app.asar "./build/electron-v0.36.4-darwin-x64/DagomaBurner.app/Contents/Resources/"
-#cp -R "./build/electron-v0.36.4-darwin-x64/DagomaBurner.app" "./build/DagomaBurner ${versionUnderscore}.app"
+asar pack src/ app.asar
+cp app.asar "./build/electron-v1.1.3-darwin-x64/DagomaBurner.app/Contents/Resources/"
+cp -R "./build/electron-v1.1.3-darwin-x64/DagomaBurner.app" "./build/DagomaBurner ${versionUnderscore}.app"
