219c347fe46ac1c35831e93e2f080b7f2e2e1918
tibus <julien.delnatte@gmail.com>
2016-09-01 12:14:40 +0200

    Rename DagomaDoctor en Dagoma

--- build.sh
+++ build.sh
@@ -18,8 +18,8 @@ echo "[INFO] Built Version: ${version}"
 
 if [ "$(uname)" == "Darwin" ]
 then
-${PLISTBUDDY} -c "Set :CFBundleShortVersionString ${version}" "./build/DagomaDoctor-darwin-x64/DagomaDoctor.app/Contents/Info.plist"
-${PLISTBUDDY} -c "Set :CFBundleVersion ${version}" "./build/DagomaDoctor-darwin-x64/DagomaDoctor.app/Contents/Info.plist"
+${PLISTBUDDY} -c "Set :CFBundleShortVersionString ${version}" "./build/Dagoma-darwin-x64/Dagoma.app/Contents/Info.plist"
+${PLISTBUDDY} -c "Set :CFBundleVersion ${version}" "./build/Dagoma-darwin-x64/Dagoma.app/Contents/Info.plist"
 fi
 
 echo "[INFO] Cleaning all previous generated zip archive..."
@@ -28,36 +28,36 @@ rm -f build/*.tar.gz
 
 ############ Windows ############
 echo "[INFO] Building Windows ..."
-rm -rf build/DagomaDoctor-win32-x64/resources/app/
-cp -R src/ build/DagomaDoctor-win32-x64/resources/app
-rm -rf build/DagomaDoctor-win32-x64/resources/app/node_modules
-cp -R build/modules/win/node_modules/ build/DagomaDoctor-win32-x64/resources/app/node_modules
-${ASAR} pack build/DagomaDoctor-win32-x64/resources/app/ build/DagomaDoctor-win32-x64/resources/app.asar
-rm -rf build/DagomaDoctor-win32-x64/resources/app/
-bash -c "cd build && zip -y -r DagomaDoctor-win32-x64.zip DagomaDoctor-win32-x64 > /dev/null"
-echo "[INFO] Built: build/DagomaDoctor-win32-x64.zip"
+rm -rf build/Dagoma-x64/resources/app/
+cp -R src/ build/Dagoma-x64/resources/app
+rm -rf build/Dagoma-x64/resources/app/node_modules
+cp -R build/modules/win/node_modules/ build/Dagoma-x64/resources/app/node_modules
+${ASAR} pack build/Dagoma-x64/resources/app/ build/Dagoma-x64/resources/app.asar
+rm -rf build/Dagoma-x64/resources/app/
+bash -c "cd build && zip -y -r Dagoma-x64.zip Dagoma-x64 > /dev/null"
+echo "[INFO] Built: build/Dagoma-x64.zip"
 
 ############ Mac ############
 echo "[INFO] Building Mac ..."
-rm -rf build/DagomaDoctor-darwin-x64/DagomaDoctor.app/Contents/Resources/app/
-cp -R src/ build/DagomaDoctor-darwin-x64/DagomaDoctor.app/Contents/Resources/app/
-rm -rf build/DagomaDoctor-darwin-x64/DagomaDoctor.app/Contents/Resources/app/node_modules
-cp -R build/modules/mac/node_modules/ build/DagomaDoctor-darwin-x64/DagomaDoctor.app/Contents/Resources/app/node_modules
-${ASAR} pack build/DagomaDoctor-darwin-x64/DagomaDoctor.app/Contents/Resources/app/ build/DagomaDoctor-darwin-x64/DagomaDoctor.app/Contents/Resources/app.asar
-rm -rf build/DagomaDoctor-darwin-x64/DagomaDoctor.app/Contents/Resources/app/
-bash -c "cd build && zip -y -r DagomaDoctor-darwin-x64.zip DagomaDoctor-darwin-x64 > /dev/null"
-echo "[INFO] Built: build/DagomaDoctor-darwin-x64.zip"
+rm -rf build/Dagoma-darwin-x64/Dagoma.app/Contents/Resources/app/
+cp -R src/ build/Dagoma-darwin-x64/Dagoma.app/Contents/Resources/app/
+rm -rf build/Dagoma-darwin-x64/Dagoma.app/Contents/Resources/app/node_modules
+cp -R build/modules/mac/node_modules/ build/Dagoma-darwin-x64/Dagoma.app/Contents/Resources/app/node_modules
+${ASAR} pack build/Dagoma-darwin-x64/Dagoma.app/Contents/Resources/app/ build/Dagoma-darwin-x64/Dagoma.app/Contents/Resources/app.asar
+rm -rf build/Dagoma-darwin-x64/Dagoma.app/Contents/Resources/app/
+bash -c "cd build && zip -y -r Dagoma-darwin-x64.zip Dagoma-darwin-x64 > /dev/null"
+echo "[INFO] Built: build/Dagoma-darwin-x64.zip"
 
 ############ Linux ############
 echo "[INFO] Building Linux ..."
-rm -rf build/DagomaDoctor-linux-x64/resources/app/
-cp -R src/ build/DagomaDoctor-linux-x64/resources/app
-rm -rf build/DagomaDoctor-linux-x64/resources/app/node_modules
-cp -R build/modules/linux/node_modules/ build/DagomaDoctor-linux-x64/resources/app/node_modules
-${ASAR} pack build/DagomaDoctor-linux-x64/resources/app/ build/DagomaDoctor-linux-x64/resources/app.asar
-rm -rf build/DagomaDoctor-linux-x64/resources/app/
-tar -czf build/DagomaDoctor-linux-x64.tar.gz -C build/ DagomaDoctor-linux-x64
-bash -c "cd build && zip -y -r DagomaDoctor-linux-x64.zip DagomaDoctor-linux-x64.tar.gz > /dev/null"
-echo "[INFO] Built: build/DagomaDoctor-linux-x64.zip"
+rm -rf build/Dagoma-linux-x64/resources/app/
+cp -R src/ build/Dagoma-linux-x64/resources/app
+rm -rf build/Dagoma-linux-x64/resources/app/node_modules
+cp -R build/modules/linux/node_modules/ build/Dagoma-linux-x64/resources/app/node_modules
+${ASAR} pack build/Dagoma-linux-x64/resources/app/ build/Dagoma-linux-x64/resources/app.asar
+rm -rf build/Dagoma-linux-x64/resources/app/
+tar -czf build/Dagoma-linux-x64.tar.gz -C build/ Dagoma-linux-x64
+bash -c "cd build && zip -y -r Dagoma-linux-x64.zip Dagoma-linux-x64.tar.gz > /dev/null"
+echo "[INFO] Built: build/Dagoma-linux-x64.zip"
 
 echo "[INFO] Build Finished ..."
