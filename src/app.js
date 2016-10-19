1321e35f9a90049217c2a6971f1ddff72aa5e39a
tibus <julien.delnatte@gmail.com>
2016-10-19 17:16:44 +0200

    Bug fix when updating application : take the appDataVersion even if we don't want to

--- src/app.js
+++ src/app.js
@@ -6,31 +6,34 @@ var mainWindow = null,
     updateWindow = null,
     requestUpdate = null;
 
-var version = require("./config.json")["build-date"];
+var version = +(require("./config.json")["build-date"]);
 if(version == "THE_BUILD_DATE")
   version = -1;
 
 var appRunner = null;
 var updateUrl = require("./config.json").updateUrl;
-var versionInAppData = false;
+var versionInAppData = 0;
+var takeVersionFromAppData = false;
 
 try {
-  versionInAppData = require(app.getPath("userData")+"/app/config.json")["build-date"];
+  versionInAppData = +(require(app.getPath("userData")+"/app/config.json")["build-date"]);
   console.log("version in Application Directory : ", version);
   console.log("version in AppData : ", versionInAppData);
   if(versionInAppData && versionInAppData != "THE_BUILD_DATE" && versionInAppData >= version){
     updateUrl = require(app.getPath("userData")+"/app/config.json").updateUrl;
     appRunner = require(app.getPath("userData")+"/app/appRunner.js");
-    versionInAppData = true;
+    takeVersionFromAppData = true;
     console.log("get Application From AppData");
   }else{
     console.log("get Application From Application Directory");
     appRunner = require("./appRunner.js");
+    takeVersionFromAppData = false;
   }
 }
 catch (e) {
   appRunner = require("./appRunner.js");
   console.log("get Application From Application Directory because of an error : ", e);
+  takeVersionFromAppData = false;
 }
 
-appRunner.run(versionInAppData, updateUrl, __dirname);
+appRunner.run(takeVersionFromAppData, updateUrl, __dirname);
