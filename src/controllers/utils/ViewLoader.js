084c859e2a7df6ad06be26fd652dc1fff5e41b21
tibus <julien.delnatte@gmail.com>
2016-11-10 18:52:18 +0100

    faster page loading

--- src/controllers/utils/ViewLoader.js
+++ src/controllers/utils/ViewLoader.js
@@ -4,14 +4,14 @@ var fs = require('fs');
 var i18n = require('../../i18n/i18n.js');
 
 module.exports = function loadFile(file, callback){
+  //console.log("loadFile",file);
   var that = this;
-  fs.readFile(__dirname + "/../../"+'views/'+file+".html", 'utf8', function (err,data){
-    if(err){
-      return loadFile(file, callback);
-    }
-    var template = Handlebars.compile(data);
-    var context = i18n.currentLanguage();
-    var html = template(context);
-    callback(html);
-  });
+  var data = fs.readFileSync(__dirname + "/../../"+'views/'+file+".html", 'utf8')
+  //console.log("file readed");
+  var template = Handlebars.compile(data);
+  //console.log("file compiled");
+  var context = i18n.currentLanguage();
+  var html = template(context);
+  //console.log("file i18n done");
+  callback(html);
 };
