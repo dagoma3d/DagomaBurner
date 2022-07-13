2fd3698ebc12d7f1f4ee980e68a55b5e0d7a28c9
orel <orel@dagoma.fr>
2022-07-13 14:38:10 +0200

    Enhance run script

--- runElectronApp.sh
+++ runElectronApp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./src/node_modules/.bin/electron src/.
+NODE_ENV=debug LD_LIBRARY_PATH=`pwd`/build/DagomApp-linux-x64 ./src/node_modules/.bin/electron src/.
 #/usr/local/lib/node_modules/electron-prebuilt/dist/Electron.app/Contents/MacOS/Electron src/.
